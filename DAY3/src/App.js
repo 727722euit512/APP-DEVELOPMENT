c611ce371dbe884e746d0d00de98595c43bf4c8d
727722euit512 <126044503+727722euit512@users.noreply.github.com>
2023-07-11 14:47:41 +0530

    DAY-3

---
+++ DAY3/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import LoginPage from './LoginPage';
+// import Header from './Header';
+// import { BrowserRouter as Router,Route,Routes } from 'react-router-dom';
+// import './login.css';
+function App() {
+  return (
+    <div className="App">
+    <LoginPage/>
+    </div>
+    
+  );
+}
+
+export default App;
