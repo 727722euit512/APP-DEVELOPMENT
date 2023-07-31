7cce9aa0e1e13da7c5ca4b322d3d9db9c8f14c14
727722euit512 <126044503+727722euit512@users.noreply.github.com>
2023-07-31 10:04:15 +0530

    Backend

---
+++ DAY11/Signupform/src/main/java/com/example/demo/model/Signup.java
@@ -0,0 +1,56 @@
+
+package com.example.demo.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+
+public class Signup {
+	@Id
+	
+	private String name;
+	private String mail;
+	private String password;
+	private String confirmPassword;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getMail() {
+		return mail;
+	}
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getConfirmPassword() {
+		return confirmPassword;
+	}
+	public void setConfirmPassword(String confirmPassword) {
+		this.confirmPassword = confirmPassword;
+	}
+	@Override
+	public String toString() {
+		return "Signup [name=" + name + ", mail=" + mail + ", password=" + password + ", confirmPassword="
+				+ confirmPassword + "]";
+	}
+	
+
+}
